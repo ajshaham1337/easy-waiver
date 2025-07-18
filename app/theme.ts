7f597dbf3756c58d4b3e897f29cd2064486cd0cb
Alex Shaham <ajshaham@gmail.com>
2025-07-17 23:34:43 -0700

    Finished dark mode. Delete old files.

--- app/theme.ts
+++ app/theme.ts
@@ -23,4 +23,19 @@ export const getTheme = (mode: 'light' | 'dark') =>
         "sans-serif",
       ].join(","),
     },
+    components: {
+      MuiPaper: {
+        variants: [
+          {
+            props: { variant: 'goldCard' },
+            style: ({ theme }) => ({
+              backgroundColor: theme.palette.background.paper,
+              border: `1px solid ${theme.palette.divider}`,
+              boxShadow: theme.shadows[3],
+              borderRadius: theme.shape.borderRadius,
+            }),
+          },
+        ],
+      },
+    },
   }); 
